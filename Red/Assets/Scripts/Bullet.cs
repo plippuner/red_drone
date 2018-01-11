ccbf15a8a5239cddd58325aed91cf43bf76d0050
plippuner <pierre.lippuner@gmail.com>
2018-01-11 09:04:53 +0100

    Bullet

---
+++ Red/Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject player = GameObject.Find("Player");
+        Transform playerTransform = player.transform;
+        // get player position
+        Vector2 position = playerTransform.position;
+        transform.position = new Vector2(position.x, position.y);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
