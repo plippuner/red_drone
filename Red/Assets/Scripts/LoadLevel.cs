259a99f2b539a25d519918a96e5803978756f5bd
Paskull <felber93@hotmail.com>
2018-01-15 11:09:19 +0100

    levelload

---
+++ Red/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadLevel : MonoBehaviour {
+
+
+ public void Recordloading()
+    {
+        SceneManager.LoadScene("Aufnahme");
+    }
+
+    public void Gameloading()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Scoreloading()
+    {
+        SceneManager.LoadScene("Score");
+    }
+
+
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
