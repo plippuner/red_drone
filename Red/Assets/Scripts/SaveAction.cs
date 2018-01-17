3a72470fc149b596cbbf369f489d11dd9b76a51d
Paskull <felber93@hotmail.com>
2018-01-17 09:01:35 +0100

    replay2

---
+++ Red/Assets/Scripts/SaveAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class SaveAction : MonoBehaviour
+{
+
+    public string Action = "None";
+    public float CurrentATime = 0;
+    public Vector3 Pos;
+
+
+
+    public SaveAction(string inAction, float inCurrentTime, Vector3 inPos)
+    {
+        CurrentATime = inCurrentTime;
+        Action = inAction;
+        Pos = inPos;
+
+}
+
+}
